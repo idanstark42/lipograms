0d4f64c169283085c874dc947d9c77230cc75ea9
Idan Stark <idanstark42@gmail.com>
2023-11-24 17:10:51 +0200

    all is working

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ import './index.css'
 import { load } from './google-sheets-api'
 import { TOPBAR_TEXTS } from './topbar-texts'
 
+function LettersBar ({ selected = [], onSelect = () => {} }) {
+  const LETTER_OPTIONS = 'אבגדהוזחטיכלמנסעפצקרשת'.split('')
+
+  return <div className='letters-bar'>
+    {LETTER_OPTIONS.map(letter => <div className={`letter ${selected.includes(letter) ? 'selected' : ''}`} onClick={() => onSelect(letter)}>{letter}</div>)}
+  </div>
+}
+
 function TopBar () {
   const [text, setText] = useState('')
   const [letter, setLetter] = useState('א')
-  const LETTER_OPTIONS = 'אבגדהוזחטיכלמנסעפצקרשת'.split('')
 
   useEffect(() => {
     setText(TOPBAR_TEXTS[letter])
@@ -17,9 +24,17 @@ function TopBar () {
 
   return <div className='top-bar'>
     <div className='text'>{text}</div>
-    <div className='letter-options'>
-      {LETTER_OPTIONS.map((letterOption) => <div className={`letter ${letterOption === letter ? 'selected' : ''}`} onClick={() => setLetter(letterOption)}>{letterOption}</div>)}
-    </div>
+    <LettersBar selected={[letter]} onSelect={setLetter} />    
+  </div>
+}
+
+function Lipogram ({ text = '', author = 'אנונימי' }) {
+  const missingLetters = 'אבגדהוזחטיכלמנסעפצקרשת'.split('').filter(letter => !text.includes(letter))
+
+  return <div className='lipogram'>
+    <div className='text'>{text}</div>
+    <div className='author'>מאת: {author}</div>
+    <LettersBar selected={missingLetters} />
   </div>
 }
 
@@ -35,10 +50,13 @@ Catalyst.init({
         }) ()
       })
       
-      return <div className='home'>
+      return <>
         <h1>ליפוגרמות</h1>
         <TopBar />
-      </div>
+        <div className='lipograms'>
+          {entries.map(entry => <Lipogram text={entry['טקסט']} author={entry['שם']} />)}
+        </div>
+      </>
     }
   },
   theme: {
